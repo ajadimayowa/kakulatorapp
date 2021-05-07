fd2dfe8cd31155b1ef6b7bd151d1bb8307abc5d0
Ajadi Mayowa <kingfloat@Ajadis-MacBook-Pro.local>
2021-05-07 18:45:16 +0100

    Application Start Point

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet ,Text,View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+export default function App(){
+  return(
+    <View style={style.viewPort}>
+      <Text>Welcome</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+const style = StyleSheet.create({
+  viewPort:{
+    flex:1,
+    backgroundColor:'#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+
+})
+
